60be55627ec64744614e42ff43b4885d2ee67a60
rajon111 <mostofarajon111@gmail.com>
2022-09-27 14:46:38 +0600

    added slider footer animation demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/Header/header/header.component';
+import { HomeComponent } from './components/Home/home/home.component';
+import { FeaturedProductsComponent } from './components/Home/home/featured-products/featured-products.component';
+import { FooterComponent } from './layout/Footer/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FeaturedProductsComponent,
+    FooterComponent,
+ 
   ],
   imports: [
     BrowserModule,
